1e1056a0de8c13e11693360121b2dd88d40aecbe
Ana <anaclimgo@gmail.com>
2026-01-23 18:06:28 -0300

    feat: added create transaction function and params for type, amount, category and occured_on

--- backend/app/controllers/api/v1/transactions_controller.rb
+++ backend/app/controllers/api/v1/transactions_controller.rb
@@ -1,2 +1,27 @@
-class Api::V1::TransactionsController < ApplicationController
+class Api::V1::TransactionsController < Api::V1::BaseController
+  def index
+    transactions = @current_user.transactions.order(occurred_on: :desc)
+    render json: transactions
+  end
+
+  def create
+    transaction = @current_user.transactions.new(transaction_params)
+
+    if transaction.save
+      render json: transaction, status: :created
+    else
+      render json: { errors: transaction.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(
+      :transaction_type,
+      :amount,
+      :category,
+      :occurred_on
+    )
+  end
 end
