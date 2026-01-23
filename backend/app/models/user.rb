fb2552d59e9724d30efd9f076cd8778984d33c0c
Ana <anaclimgo@gmail.com>
2026-01-23 17:59:41 -0300

    feat: added devise to user model and auth token generator

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
+
+  before_create :generate_auth_token
+
+  private
+
+  def generate_auth_token
+    self.auth_token = SecureRandom.hex(20)
+  end
 end
