50456bf31c69cf87c3c5029ced6fd8a6508e3172
Ana <anaclimgo@gmail.com>
2026-01-23 18:07:49 -0300

    feat: added transactions routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get "health", to: "health#index"
+
+      # Transactions routes
+      resources :transactions, only: [:index, :create]
     end
   end
 end
