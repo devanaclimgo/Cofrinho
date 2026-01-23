b3d48df639f6c7c7cd654be193981aaa9b652566
Ana <anaclimgo@gmail.com>
2026-01-23 18:03:50 -0300

    feat: added transaction type with income and expense and validations of amount, category, occured_on and presence

--- backend/app/models/transaction.rb
+++ backend/app/models/transaction.rb
@@ -1,3 +1,10 @@
 class Transaction < ApplicationRecord
   belongs_to :user
+
+  enum transaction_type: {
+    income: "income",
+    expense: "expense"
+  }
+
+  validates :amount, :category, :occurred_on, presence: true
 end
