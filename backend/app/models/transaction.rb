84af31289273d5b4aaa210ea1bbf948bd0cdca09
Ana <anaclimgo@gmail.com>
2026-01-25 17:36:17 -0300

    feat: added clear analytics cache in transaction model

--- backend/app/models/transaction.rb
+++ backend/app/models/transaction.rb
@@ -7,4 +7,10 @@ class Transaction < ApplicationRecord
   }
 
   validates :amount, :category, :occurred_on, presence: true
+
+  after_commit :clear_analytics_cache
+
+  def clear_analytics_cache
+    Rails.cache.delete_matched("analytics:*:user:#{user_id}:*")
+  end
 end
