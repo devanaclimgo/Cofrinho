95079299bafcfe9e057f82075f4e87a1231b580a
Ana <anaclimgo@gmail.com>
2026-01-25 17:35:20 -0300

    feat: implemented caching performance in weekly analytics

--- backend/app/services/analytics/weekly_service.rb
+++ backend/app/services/analytics/weekly_service.rb
@@ -6,11 +6,26 @@ module Analytics
     end
 
     def call
+      Rails.cache.fetch(cache_key, expires_in: 12.hours) do
+        calculate
+      end
+    end
+
+    private
+
+    def calculate
+      start_date = 1.week.ago.beginning_of_day
+      end_date = Date.today.end_of_day
+
       @user.transactions
            .where(transaction_type: @transaction_type)
-           .where(occurred_on: 1.week.ago.beginning_of_day..Date.today)
+           .where(occurred_on: start_date..end_date)
            .group("DATE(occurred_on)")
            .sum(:amount)
+    end
+
+    def cache_key
+      "analytics:weekly:user:#{@user.id}:#{@transaction_type}"
     end
   end
 end
