aaacf630158b0d6fe8261c0d53ad2810ef1d13c4
Ana <anaclimgo@gmail.com>
2026-01-25 17:35:28 -0300

    feat: implemented caching performance in yearly analytics

--- backend/app/services/analytics/yearly_service.rb
+++ backend/app/services/analytics/yearly_service.rb
@@ -7,6 +7,14 @@ module Analytics
     end
 
     def call
+      Rails.cache.fetch(cache_key, expires_in: 12.hours) do
+        calculate
+      end
+    end
+
+    private
+
+    def calculate
       start_date = Date.new(@year, 1, 1)
       end_date   = start_date.end_of_year
 
@@ -15,6 +23,10 @@ module Analytics
            .where(occurred_on: start_date..end_date)
            .group("DATE_TRUNC('month', occurred_on)")
            .sum(:amount)
+    end
+
+    def cache_key
+      "analytics:yearly:user:#{@user.id}:#{@transaction_type}:#{@year}"
     end
   end
 end
