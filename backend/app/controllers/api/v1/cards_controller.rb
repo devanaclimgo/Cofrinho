813ad743b5af99a83e21f3b16c70a5fdfed334c9
Ana <anaclimgo@gmail.com>
2026-02-01 11:41:55 -0300

    feat: added cards controller logic to create card and permition params

--- backend/app/controllers/api/v1/cards_controller.rb
+++ backend/app/controllers/api/v1/cards_controller.rb
@@ -1,2 +1,21 @@
-class Api::V1::CardsController < ApplicationController
+class Api::V1::CardsController < Api::V1::BaseController
+  def index
+    render json: @current_user.cards
+  end
+
+  def create
+    card = @current_user.cards.new(card_params)
+
+    if card.save
+      render json: card, status: :created
+    else
+      render json: { errors: card.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:name, :limit, :closing_day, :due_day)
+  end
 end
