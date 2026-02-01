296782d387949b0cdfafc70875b8a6e082a2ff7d
Ana <anaclimgo@gmail.com>
2026-02-01 11:35:36 -0300

    feat: added card purchase installment validation logic

--- backend/app/models/installment.rb
+++ backend/app/models/installment.rb
@@ -1,3 +1,8 @@
 class Installment < ApplicationRecord
   belongs_to :card_purchase
+
+  delegate :card, to: :card_purchase
+  delegate :user, to: :card
+
+  validates :amount, :month, presence: true
 end
