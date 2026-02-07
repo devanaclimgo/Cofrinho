9779724ee1a3dc4b21f31716365fca16fb868775
Ana <anaclimgo@gmail.com>
2026-02-07 20:55:35 -0300

    feat: created purchase validations controller logic

--- backend/app/controllers/api/v1/purchase_validations_controller.rb
+++ backend/app/controllers/api/v1/purchase_validations_controller.rb
@@ -1,2 +1,15 @@
-class Api::V1::PurchaseValidationsController < ApplicationController
+class Api::V1::PurchaseValidationsController < Api::V1::BaseController
+  def validate
+    card = @current_user.cards.find(params[:card_id])
+
+    result = Finance::SmartPurchaseValidator.new(
+      user: @current_user,
+      card: card,
+      total_amount: params[:total_amount],
+      installments_count: params[:installments_count],
+      purchased_on: params[:purchased_on]
+    ).call
+
+    render json: result
+  end
 end
