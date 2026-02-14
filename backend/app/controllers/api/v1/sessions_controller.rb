53c4d1b59662013128ab12afd2a6811fb195d563
Ana <anaclimgo@gmail.com>
2026-02-14 11:39:49 -0300

    feat: added status messages

--- backend/app/controllers/api/v1/sessions_controller.rb
+++ backend/app/controllers/api/v1/sessions_controller.rb
@@ -5,11 +5,22 @@ class Api::V1::SessionsController < Devise::SessionsController
 
   def respond_with(resource, _opts = {})
     render json: {
+      status: { code: 200, message: "Login realizado com sucesso" },
       user: resource
     }, status: :ok
   end
 
   def respond_to_on_destroy
-    head :no_content
+    if current_user
+      render json: {
+        status: 200,
+        message: "Logout realizado com sucesso"
+      }, status: :ok
+    else
+      render json: {
+        status: 401,
+        message: "Usuário não encontrado"
+      }, status: :unauthorized
+    end
   end
 end
