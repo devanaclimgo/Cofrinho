b7a9fab57a9464a6cd4abca21cf8bd8c730eec75
Ana <anaclimgo@gmail.com>
2026-06-19 19:54:23 -0300

    feat: implement InstallmentGenerator service for creating installments from a transaction

---
+++ backend/app/services/installment_generator.service.rb
@@ -0,0 +1,21 @@
+class InstallmentGenerator
+
+  def self.call(transaction:, total_installments:)
+
+    value = transaction.amount / total_installments
+
+    total_installments.times do |i|
+
+      Installment.create!(
+        transaction: transaction,
+        amount: value,
+        current_installment: i + 1,
+        total_installments: total_installments,
+        due_date: Date.today + i.months
+      )
+
+    end
+
+  end
+
+end
