8d61d4fa193c9b8b703eb4bd4ecac55d2a05d941
Ana <anaclimgo@gmail.com>
2026-06-22 19:36:16 -0300

    feat: add AuthHelper module for generating JWT authorization headers

---
+++ backend/spec/support/auth_helper.rb
@@ -0,0 +1,21 @@
+module AuthHelper
+
+  def auth_headers(user)
+
+    token =
+      Warden::JWTAuth::UserEncoder
+      .new
+      .call(
+        user,
+        :user,
+        nil
+      )
+      .first
+
+    {
+      "Authorization" =>
+      "Bearer #{token}"
+    }
+  end
+
+end
