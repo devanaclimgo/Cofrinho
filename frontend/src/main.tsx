1fe37220dc7824eca3e136afd7f4ebadb276a722
Ana <anaclimgo@gmail.com>
2026-07-12 17:24:39 -0300

    feat: restructure main.tsx to include QueryProvider for improved data handling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,25 +1,28 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import "./index.css"
-import { App } from "./App"
-import { ThemeProvider } from "./lib/ThemeContext"
-import { I18nProvider } from "./i18n/I18nContext"
-import { CurrencyProvider } from "./lib/CurrencyContext"
-import { AppStateProvider } from "./lib/AppStateContext"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import { App } from "./App";
+import { ThemeProvider } from "./lib/ThemeContext";
+import { I18nProvider } from "./i18n/I18nContext";
+import { CurrencyProvider } from "./lib/CurrencyContext";
+import { AppStateProvider } from "./lib/AppStateContext";
+import { QueryProvider } from "./providers/QueryProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <I18nProvider>
-        <CurrencyProvider>
-          <AppStateProvider>
-            <BrowserRouter>
-              <App />
-            </BrowserRouter>
-          </AppStateProvider>
-        </CurrencyProvider>
-      </I18nProvider>
-    </ThemeProvider>
+    <QueryProvider>
+      <ThemeProvider>
+        <I18nProvider>
+          <CurrencyProvider>
+            <AppStateProvider>
+              <BrowserRouter>
+                <App />
+              </BrowserRouter>
+            </AppStateProvider>
+          </CurrencyProvider>
+        </I18nProvider>
+      </ThemeProvider>
+    </QueryProvider>
   </StrictMode>,
-)
+);
