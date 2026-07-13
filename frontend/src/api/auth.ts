613ead3fe7165850cf0d6a788af54aeae863aa31
Ana <anaclimgo@gmail.com>
2026-07-13 15:52:18 -0300

    refactor: convert signup and login functions to async/await syntax and define data interfaces

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,18 +1,32 @@
-import { api } from "./axios"
+import { api } from "./axios";
 
-export const signup = (data: {
-  name: string
-  email: string
-  password: string
-}) =>
-  api.post("/signup", {
-    user: data,
-  })
+export interface LoginData {
+  email: string;
+  password: string;
+}
+export interface SignupData {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+}
 
-export const login = (data: {
-  email: string
-  password: string
-}) =>
-  api.post("/login", {
+export async function login(data: LoginData) {
+  const response = await api.post("/login", {
     user: data,
-  })
+  });
+
+  return response;
+}
+
+export async function signup(data: SignupData) {
+  const response = await api.post("/signup", {
+    user: data,
+  });
+
+  return response;
+}
+
+export async function logout() {
+  return api.delete("/logout");
+}
