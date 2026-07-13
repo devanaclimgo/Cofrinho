9c6522e67a7c3d740ff709ee221a30436746603a
Ana <anaclimgo@gmail.com>
2026-07-13 15:49:51 -0300

    feat: update axios configuration to use API_URL and add request interceptor for token management

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,8 +1,20 @@
 import axios from "axios"
+import { API_URL } from "../lib/constants"
+import { storage } from "../lib/storage"
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
+})
+
+api.interceptors.request.use((config) => {
+  const token = storage.getToken()
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
 })
