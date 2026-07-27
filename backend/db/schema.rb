a071681b57d3acb993f61edbd2146aa28ea13f5d
Ana <anaclimgo@gmail.com>
2026-07-27 13:22:22 -0300

    refactor: update schema to include new fields in transactions for enhanced data structure

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_20_190344) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_27_161745) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -72,12 +72,16 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_20_190344) do
     t.string "category"
     t.datetime "created_at", null: false
     t.string "description"
+    t.string "icon"
     t.integer "kind"
+    t.integer "status"
     t.date "transaction_date"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.bigint "wallet_id", null: false
     t.index ["card_id"], name: "index_transactions_on_card_id"
     t.index ["user_id"], name: "index_transactions_on_user_id"
+    t.index ["wallet_id"], name: "index_transactions_on_wallet_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -114,5 +118,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_20_190344) do
   end
 
   add_foreign_key "goals", "users"
+  add_foreign_key "transactions", "wallets"
   add_foreign_key "wallets", "users"
 end
