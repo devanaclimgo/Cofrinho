c5bee1a16d355f4e860f646df93324a500b5fd54
Ana <anaclimgo@gmail.com>
2026-07-27 13:21:36 -0300

    refactor: update index and show actions in TransactionsController for user-specific transaction retrieval

--- backend/app/controllers/api/v1/transactions_controller.rb
+++ backend/app/controllers/api/v1/transactions_controller.rb
@@ -1,12 +1,15 @@
 class Api::V1::TransactionsController < ApplicationController
   before_action :authenticate_user!
 
-  def show
-    transaction = Transaction.find(params[:id])
+  def index
+    transactions = current_user.transactions.order(transaction_date: :desc)
 
-    authorize transaction
+    render json: TransactionSerializer.new(transactions)
+  end
+
+  def show
+    transaction = current_user.transactions.find(params[:id])
 
     render json: TransactionSerializer.new(transaction)
   end
-
 end
