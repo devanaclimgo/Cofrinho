45e79b46fc3a4d5827148f8c1cce1811ebefc957
Ana <anaclimgo@gmail.com>
2026-08-04 13:56:59 -0300

    feat: add resources for users in API v1 routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
+
+      resources :users, only: [:create, :update, :destroy]
       
       resources :transactions
       resources :cards
