ddff1d1d4a3b111ddccfcd861fc852c31f901e93
Ana <anaclimgo@gmail.com>
2026-08-04 16:19:42 -0300

    feat: remove default attributes from card factory

--- backend/spec/factories/cards.rb
+++ backend/spec/factories/cards.rb
@@ -1,11 +1,5 @@
 FactoryBot.define do
   factory :card do
-    user
-
-    name { "Nubank" }
-    color { "purple" }
-    limit { 3000 }
-    closing_day { 20 }
-    due_day { 27 }
+    
   end
 end
