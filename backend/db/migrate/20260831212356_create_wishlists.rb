9b16cc8fdba60ff3bd5d6aa80b72da61ab92c551
Ana <anaclimgo@gmail.com>
2026-08-31 18:57:33 -0300

    feat: update Wishlist model to replace verdict with priority attribute

--- backend/db/migrate/20260831212356_create_wishlists.rb
+++ backend/db/migrate/20260831212356_create_wishlists.rb
@@ -7,7 +7,7 @@ class CreateWishlists < ActiveRecord::Migration[8.1]
       t.decimal :price, precision: 12, scale: 2, null: false
       t.string :image
       t.date :desired_date
-      t.string :verdict
+      t.string :priority
 
       t.timestamps
     end
