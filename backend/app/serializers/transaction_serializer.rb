94f9486ae41cd258984f57b897a5c66b9b20304d
Ana <anaclimgo@gmail.com>
2026-09-06 12:59:11 -0300

    refactor: update TransactionSerializer attributes and methods for clarity

--- backend/app/serializers/transaction_serializer.rb
+++ backend/app/serializers/transaction_serializer.rb
@@ -1,19 +1,7 @@
 class TransactionSerializer < ActiveModel::Serializer
-    attributes :id, :description, :category, :wallet_id, :card_id, :amount, :kind, :status, :transaction_date
+  attributes :id, :description, :category, :category_id, :wallet_id, :amount, :kind, :status, :transaction_date
 
-  def title
-    object.description
-  end
-
-  def wallet
-    object.wallet&.name
-  end
-
-  def type
-    object.kind
-  end
-
-  def date
-    object.transaction_date
+  def category
+    object.category&.name
   end
 end
