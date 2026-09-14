d99eeaaea55eddd90e67ad6ba00c625269a123bb
Ana <anaclimgo@gmail.com>
2026-09-14 10:10:30 -0300

    feat: implement update action for user profile in UsersController

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -4,4 +4,18 @@ class Api::V1::UsersController < ApplicationController
   def me
     render json: current_user
   end
+
+  def update
+    if current_user.update(user_params)
+      render json: current_user
+    else
+      render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
